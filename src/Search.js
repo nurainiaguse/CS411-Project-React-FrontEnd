82031b6a71597510eb84fe2924d4170263ce1ae9
nurainiaguse <aguse2@illinois.edu>
2019-03-05 20:31:55 -0600

    Can perform simple search

--- src/Search.js
+++ src/Search.js
@@ -11,11 +11,19 @@ function Item(props){
      		</div>
      	)
      }
+     else if(props.item == 'No results'){
+     	return (
+			<div>
+				<h3>We couldn't find a restaurant with that name!</h3>
+			</div>
+
+		)
+     }
     else{
 		return (
 			<div>
-				<h3>Name: {props.item.name}</h3>
-				<h3>Rating: {props.item.id}</h3>
+				<h3>We found: {props.item["DBA Name"]}</h3>
+
 			</div>
 
 		)
@@ -31,7 +39,7 @@ class Search extends Component {
 			item: {},
 		};
 
-		this.baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+		this.baseUrl = 'http://localhost:5000/api/restaurant/';
 		this.inputChangeHandler = this.inputChangeHandler.bind(this);
 		this.onClickHandler = this.onClickHandler.bind(this);
 	}
@@ -46,8 +54,9 @@ class Search extends Component {
 		axios.get(url).then((response) => {
 			this.setState({
 				item: response.data
+				
 			});
-
+			console.log(response.data);
 		}).catch((error) => {
 			console.log(error);
 		});
